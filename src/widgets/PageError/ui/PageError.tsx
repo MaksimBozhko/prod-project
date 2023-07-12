6249e1144342f535cbdc10593d5c788da1351859
MaksimBozhko <bochkomax2000@gmail.com>
2023-07-12 13:28:27 +0300

    fix: style

--- src/widgets/PageError/ui/PageError.tsx
+++ src/widgets/PageError/ui/PageError.tsx
@@ -1,7 +1,6 @@
 import classNames from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Button } from 'shared/ui/Button/Button';
-import cls from './PageError.module.scss'
 
 interface PageErrorProps {
     className?: string
@@ -14,7 +13,7 @@ export const PageError = ({ className }: PageErrorProps) => {
     location.reload()
   }
   return (
-    <div className={classNames(cls.PageError, {}, [className])}>
+    <div className={classNames('', {}, [className])}>
       {console.log(t('Произошла ошибка'))}
       {t('Произошла ошибка')}
       <Button onClick={reloadPage}>
