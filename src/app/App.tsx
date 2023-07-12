42c01fbb46a295edd98854bdb70efb8092b3a1b1
MaksimBozhko <bochkomax2000@gmail.com>
2023-07-12 20:34:41 +0300

    fix: style

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,10 +3,12 @@ import cn from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { useTheme } from 'app/providers/ThemeProvider';
 
 export const App = () => {
+  const { theme } = useTheme()
   return (
-    <div className={cn('app', {}, [])}>
+    <div className={cn('app', {}, [theme])}>
       <Suspense fallback="">
         <Navbar />
         <div className="content-page">
