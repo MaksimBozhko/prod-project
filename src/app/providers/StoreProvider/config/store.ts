72393fe7aa0c0e9a424a7af803bfdd4fba7bea9f
MaksimBozhko <bochkomax2000@gmail.com>
2023-07-17 15:15:56 +0300

    add: instance api, env api global

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -2,11 +2,15 @@ import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
 import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
+import { $api } from 'shared/api/api';
+import { NavigateOptions } from 'react-router';
+import { To } from 'react-router-dom';
 import { createReducerManager } from './reducerManager';
 
 export function createReduxStore(
   initialState?: StateSchema,
   asyncReducers?: ReducersMapObject<StateSchema>,
+  navigate?: (to: To, options?: NavigateOptions) => void,
 ) {
   const rootReducers: ReducersMapObject<StateSchema> = {
     ...asyncReducers,
@@ -16,10 +20,18 @@ export function createReduxStore(
 
   const reducerManager = createReducerManager(rootReducers)
 
-  const store = configureStore<StateSchema>({
+  const store = configureStore({
     reducer: reducerManager.reduce,
     preloadedState: initialState,
     devTools: __IS_DEV__,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+      thunk: {
+        extraArgument: {
+          api: $api,
+          navigate,
+        },
+      },
+    }),
   })
 
   // @ts-ignore
