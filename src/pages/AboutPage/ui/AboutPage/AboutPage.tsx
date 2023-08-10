048f1e6577aa58056217c25618f3618f1b93819d
MaksimBozhko <bochkomax2000@gmail.com>
2023-08-10 20:34:08 +0300

    add: login modal

--- src/pages/AboutPage/ui/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage/AboutPage.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from 'widgets/Page/Page';
+import { LoginModal } from 'features/AuthByUserName';
 
 const AboutPage = () => {
   const { t } = useTranslation('about');
+
+  const [isAuthModal, setIsAuthModal] = useState(false);
+  const onCloseModal = useCallback(() => {
+    setIsAuthModal(false);
+  }, []);
   return (
     <Page>
       {t('О сайте')}
+      <LoginModal
+        isOpen={isAuthModal}
+        onClose={onCloseModal}
+      />
     </Page>
   );
 };
